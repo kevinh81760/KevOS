bb086ea5d642b99a2ea43ed7c222b05e4b7df2a9
kevinh81760 <kevinh81760@gmail.com>
2026-02-06 20:54:54 -0800

    chore: tweaks

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ import ViewportScrollFade from "@/components/animations/ViewportScrollFade";
 
 export default function GalleryPage() {
   return (
-    <div className="min-h-[calc(100vh-100px)] px-6 pt-[50px] pb-20">
+    <div className="min-h-[calc(100vh-100px)] px-6 pt-[30px] pb-20">
       <div className="max-w-7xl mx-auto ml-[87px] mr-[87px]">
         <AboutIntro />
 
